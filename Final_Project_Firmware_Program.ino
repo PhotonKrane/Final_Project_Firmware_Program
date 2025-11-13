edb365f3baf6f944f7de2e9e1906af11102567b5
Isaac Correa Sousa <correaisaac887@gmail.com>
2025-11-13 19:20:39 -0300

    Update Final_Project_Firmware_Program.ino
    Update display clear

--- Final_Project_Firmware_Program.ino
+++ Final_Project_Firmware_Program.ino
@@ -318,7 +318,7 @@ void updateFloorState(int floor) {
           floors[floor].write(stopAngles[floor]);
           floorStates[floor] = RETURNING;
           floorTimers[floor] = millis();
-          display.fillRect(0, 0, 240, 60, ST77XX_WHITE);
+          display.fillRect(0, 0, 240, 30, ST77XX_WHITE);
         }
       }
       Serial.print("Sensor Hall -> ");
@@ -625,5 +625,6 @@ void eepromCancel() {
     delay(3000);
   }
 }  // Limpeza do User
+
 
 
